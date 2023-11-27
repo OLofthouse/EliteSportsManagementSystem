5cd4627388aef36907015dc8340eaf4381619103
Flo Ashworth (s5520809) <117479511+Nifteea@users.noreply.github.com>
2023-11-27 12:21:06 +0000

    updated trainingschedule - can click on slots

--- static/js/trainingschedule.js
+++ static/js/trainingschedule.js
@@ -1,3 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var cells = document.querySelectorAll('td');
+    cells.forEach(function (cell) {
+        cell.addEventListener('click', function () {
+            openPopup();
+            var cellContent = this.innerHTML;
+            var cellId = this.id;
+
+            // Set the selected time and day based on the cellId
+            var timeIndex = cellId.split(' ')[1];
+            var dayIndex = cellId.split(' ')[0];
+
+            document.getElementById('time').value = getTimeByIndex(timeIndex);
+            document.getElementById('day').value = getDayByIndex(dayIndex);
+            document.getElementById('trainingClass').value = cellContent;
+        });
+    });
+});
+
 function openPopup() {
     document.getElementById('classPopup').style.display = 'block';
 }
@@ -23,6 +42,22 @@ function saveClass() {
 
     // Close the popup
     closePopup();
+}
+
+function getTimeByIndex(index) {
+    var times = [
+        "9:00 AM - 10:30 AM",
+        "11:00 AM - 12:30 PM",
+        "1:00 PM - 2:30 PM",
+        "3:00 PM - 4:30 PM",
+        "5:00 PM - 6:30 PM"
+    ];
+    return times[index];
+}
+
+function getDayByIndex(index) {
+    var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    return days[index];
 }
 
 function getTimeIndex(selectedTime) {
