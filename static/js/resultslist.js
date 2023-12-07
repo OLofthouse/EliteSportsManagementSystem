79175bd885f1d5d491f13917bf001a2595951a58
Oscar <oscarvlofthouse@gmail.com>
2023-12-07 12:28:37 +0000

    Polishing

--- static/js/resultslist.js
+++ static/js/resultslist.js
@@ -1,4 +1,5 @@
 var currentType = ""; 
+var jsonData = {}; 
 
 /* This function will set the current user type to either coach or player */
 function getCurrentType() {
@@ -19,6 +20,59 @@ function getCurrentType() {
 
 }
 
+/* This function should send an XMLHttpRequest to the server to get all of the 
+training schedule data. Not sure as yet of the form of the data. */
+function getData() {
+
+  console.log('Getting data from Flask'); 
+
+  //Create a HTTP request to talk to the Flask app. 
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          jsonData = JSON.parse(this.responseText);
+          console.log(jsonData);
+          loadData(); 
+      } else {
+          console.log('xhttp request problem occurred', this.status); 
+      }
+  }
+
+  xhttp.open("GET", "api/users", true); 
+  xhttp.send(); 
+
+}
+
+/* This function should load the data in to the fixture list */
+function loadData() {
+  console.log("Loading Data"); 
+
+  document.getElementById('r1-tn1').innerHTML = jsonData.results[0].team1;
+  document.getElementById('r1-tn2').innerHTML = jsonData.results[0].team2;
+  document.getElementById('r1-mn').innerHTML = (jsonData.results[0].team1 + " v " + jsonData.results[0].team2); 
+  document.getElementById('r1-d').innerHTML = jsonData.results[0].date;
+  document.getElementById('r1-t').innerHTML = jsonData.results[0].time;
+  document.getElementById('r1-ts1').innerHTML = jsonData.results[0].team1Score;
+  document.getElementById('r1-ts2').innerHTML = jsonData.results[0].team2Score;
+
+  document.getElementById('r2-tn1').innerHTML = jsonData.results[1].team1;
+  document.getElementById('r2-tn2').innerHTML = jsonData.results[1].team2;
+  document.getElementById('r2-mn').innerHTML = (jsonData.results[1].team1 + " v " + jsonData.results[1].team2); 
+  document.getElementById('r2-d').innerHTML = jsonData.results[1].date;
+  document.getElementById('r2-t').innerHTML = jsonData.results[1].time; 
+  document.getElementById('r2-ts1').innerHTML = jsonData.results[1].team1Score;
+  document.getElementById('r2-ts2').innerHTML = jsonData.results[1].team2Score; 
+
+  document.getElementById('r3-tn1').innerHTML = jsonData.results[2].team1;
+  document.getElementById('r3-tn2').innerHTML = jsonData.results[2].team2;
+  document.getElementById('r3-mn').innerHTML = (jsonData.results[2].team1 + " v " + jsonData.results[2].team2); 
+  document.getElementById('r3-d').innerHTML = jsonData.results[2].date;
+  document.getElementById('r3-t').innerHTML = jsonData.results[2].time;
+  document.getElementById('r3-ts1').innerHTML = jsonData.results[2].team1Score;
+  document.getElementById('r3-ts2').innerHTML = jsonData.results[2].team2Score; 
+}
+
+
 /* This function should determine the user type (coach, player) and return to relevant dashboard. */
 function returnToDashboard() {
  
@@ -33,3 +87,5 @@ function returnToDashboard() {
 }
 
 document.getElementById('return-to-dashboard').addEventListener("click", returnToDashboard);
+
+window.onload = getData; 
